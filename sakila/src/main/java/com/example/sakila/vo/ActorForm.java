4c4ba7bbc1e61ed9c3ad8c456e326505fce3ae4e
DESKTOP-4SJR2LC\hsw52 <hsw52233@gmail.com>
2024-11-07 12:20:36 +0900

    11/7 오전

--- sakila/src/main/java/com/example/sakila/vo/ActorForm.java
+++ sakila/src/main/java/com/example/sakila/vo/ActorForm.java
@@ -1,3 +1,4 @@
+
 package com.example.sakila.vo;
 
 import java.util.List;
@@ -8,7 +9,8 @@ import lombok.Data;
 
 @Data
 public class ActorForm {
+	private int actorId;
 	private String firstName;
 	private String lastName;
-	private List<MultipartFile> actorFile;
+	private List<MultipartFile> actorFile; // MultipartFile은 파일 업로드를 처리하는 데 사용되는 framework이다.
 }
